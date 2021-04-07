76eff7a0318182d023172d3446b2bd9b2eb33950
LisaUhl02 <7061@htl.rennweg.at>
2021-04-07 13:48:18 +0200

    v4_heredoc

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
     $hotels = [
-        '###Hotel1###' => '<h1>Caesars Palace</h1>
-                           Dieses berühmte Casino-Hotel befindet sich in einem weitläufigen Gebäudekomplex im römischen Stil am lebhaften Las Vegas Strip. Es ist 1 Meile (1,6 km) von der Interstate 15 und 14 Meilen (22,5 km) vom Rio Secco Golf Club entfernt.',
+        '###Hotel1###' => <<<HOTEL1
+                               <h1>Caesars Palace</h1>
+                               Dieses berühmte Casino-Hotel befindet sich in einem weitläufigen Gebäudekomplex im römischen Stil am lebhaften Las Vegas Strip. Es ist 1 Meile (1,6 km) von der Interstate 15 und 14 Meilen (22,5 km) vom Rio Secco Golf Club entfernt.
+                               HOTEL1,
 
-        '###Hotel2###' => '<h1>Luxor</h1>
-                           Dieses farbenfrohe Casino-Resort im ägyptischen Stil liegt am südlichen Ende des Las Vegas Strip in einer 30-stöckigen Pyramide, auf deren Spitze sich ein 315.000-Watt-Scheinwerfer befindet. Zum Flughafen McCarran International sind es 2 Meilen (3,2 km).',
+        '###Hotel2###' => <<<HOTEL2
+                               <h1>Luxor</h1>
+                               Dieses farbenfrohe Casino-Resort im ägyptischen Stil liegt am südlichen Ende des Las Vegas Strip in einer 30-stöckigen Pyramide, auf deren Spitze sich ein 315.000-Watt-Scheinwerfer befindet. Zum Flughafen McCarran International sind es 2 Meilen (3,2 km).
+                               HOTEL2,
 
-        '###Hotel3###' => '<h1>The Mirage</h1>
-                           Dieses legendäre Casino-Hotel mit Blick auf den berühmten "ausbrechenden Vulkan" befindet sich am Las Vegas Strip neben dem Hotel Caesars Palace und gegenüber von Madame Tussauds Las Vegas.'
+        '###Hotel3###' => <<<HOTEL3
+                               <h1>The Mirage</h1>
+                               Dieses legendäre Casino-Hotel mit Blick auf den berühmten "ausbrechenden Vulkan" befindet sich am Las Vegas Strip neben dem Hotel Caesars Palace und gegenüber von Madame Tussauds Las Vegas.
+                               HOTEL3
     ];
 
     $html = readTemplateFile('./templates/index.html');
@@ -38,7 +44,6 @@ $hotels = array(
     new hotel("Caesars Palace", "Dieses berühmte Casino-Hotel befindet sich in einem weitläufigen Gebäudekomplex im römischen Stil am lebhaften Las Vegas Strip. Es ist 1 Meile (1,6 km) von der Interstate 15 und 14 Meilen (22,5 km) vom Rio Secco Golf Club entfernt."),
     new hotel("Luxor", "Dieses farbenfrohe Casino-Resort im ägyptischen Stil liegt am südlichen Ende des Las Vegas Strip in einer 30-stöckigen Pyramide, auf deren Spitze sich ein 315.000-Watt-Scheinwerfer befindet. Zum Flughafen McCarran International sind es 2 Meilen (3,2 km)."),
     new hotel("The Mirage", "Dieses legendäre Casino-Hotel mit Blick auf den berühmten \"ausbrechenden Vulkan\" befindet sich am Las Vegas Strip neben dem Hotel Caesars Palace und gegenüber von Madame Tussauds Las Vegas."),
-
 );
 
 echo $twig->render(
